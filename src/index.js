e6fbcf251a87cae94c76cac691e628a955b775de
Dinh Lam Dang <lam.dangdinh98@gmail.com>
2020-12-27 17:16:38 -0500

    Remove warnings

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HomeScreen from './screens/HomeScreen';
 import reportWebVitals from './reportWebVitals';
 import App from './App'
 import 'react-mdl/extra/material.css';
